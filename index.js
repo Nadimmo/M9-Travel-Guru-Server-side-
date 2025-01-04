f2f5d591d0beac79ad4b41115fe02c43a3d926bf
nadimmo <nadimmostafa334@gmail.com>
2025-01-04 21:01:04 +0600

    add package related all api

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors(
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rrkijcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,13 +29,39 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    const CollectionOfPackeges = client.db("TravelGuru").collection("packegeDB");
+    // await client.connect();
+    //package related api
+    app.post('/packages', async(req,res)=>{
+        const newPackage = req.body;
+        const result = await CollectionOfPackeges.insertOne(newPackage);
+        res.send(result);
+    })
+    app.get('/packages', async(req,res)=>{
+        const packages = req.body;
+        const result = await CollectionOfPackeges.find(packages).toArray();
+        res.send(result);
+    })
+
+    app.get('/packages/:id', async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const result = await CollectionOfPackeges.findOne(filter);
+        res.send(result);
+    })
+
+    app.delete('/packages/:id', async(req,res)=>{
+        const id = req.params.id;
+        const result = await CollectionOfPackeges.deleteOne({_id:id});
+        res.send(result);
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
