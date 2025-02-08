3125c2f0dc980bd50d50120ce851bdf0ae0ee5f5
nadimmo <nadimmostafa334@gmail.com>
2025-02-08 15:01:25 +0600

    added  delete related  user  api

--- index.js
+++ index.js
@@ -161,6 +161,12 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await CollectionOfAllUsers.deleteOne(filter);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
